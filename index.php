3dd4748ca720587327996d71a54802e542d672a6
root <root@testvm>
2023-09-30 20:02:17 +0000

    mychanges

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+// Database connection
+$servername = "localhost"; // Change this if your database is hosted elsewhere
+$username = "your_user"; // Change this to your database username
+$password = "your_password"; // Change this to your database password
+$dbname = "mywebsite_db"; // Change this to your database name
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// User registration
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Hash the password (you should use password_hash() in a production environment)
+    $hashed_password = md5($password);
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashed_password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
