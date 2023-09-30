ada5035c51813cd196d7cc3ea148142d83d91b7f
root <root@testvm>
2023-09-30 21:40:53 +0000

    testscript

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM debian 
+# TZ=America/New_York
+RUN apt-get update
+RUN apt-get install -y nginx vim curl
+RUN apt-get install -y mariadb-server 
+RUN apt-get install -y php php-fpm git
+RUN git clone https://github.com/mohamad0880218/test_project.git
+RUN cp -r ./test_project/ /var/www/html/
+RUN mv ./test_project/default /etc/nginx/sites-available/default
+RUN service mariadb start
+RUN mariadb -e "CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypassword';" \
+    && mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'myuser'@'localhost';" \
+    && mariadb -e "FLUSH PRIVILEGES;"
+RUN apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# Expose MariaDB default port
+EXPOSE 3306
+
+# Start MariaDB service when the container runs
+CMD ["mysqld"]
+CMD ["nginx","-g","daemon off;"]
+
---
+++ script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Check if MariaDB is installed
+if ! command -v mysql &> /dev/null; then
+    echo "MariaDB is not installed. Please install it first."
+    exit 1
+fi
+
+# Variables
+DB_NAME="mywebsite_db"
+DB_USER="your_user"
+DB_PASS="your_password"
+
+# Enable MariaDB (if not already enabled)
+sudo systemctl start mariadb
+sudo systemctl stop mariadb
+sudo systemctl enable nginx
+sudo mysqld_safe --skip-grant-tables &
+
+sleep 5
+
+
+sudo mariadb -u $DB_USER -p $DB_PASS <<EOF
+	FLUSH PRIVILEGES;
+	ALTER USER 'root'@localhost IDENTIFIED BY 'root_password';II
+	FLUSH PRIVILEGES;
+	CREATE DATABASE mywebsite;
+	CREATE USER 'your_user'@localhost IDENTIFIED BY 'your_password';
+	GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'localhost';
+	FLUSH PRIVILEGES;
+	
+EOF
+sudo systemctl stop mariadb;
+sudo systemctl start mariadb;
+sudo mariadb -u $DB_USER -p $DB_PASSWORD <<EOF
+	USE mywebsite_db;
+	CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255),
+        password VARCHAR(255)
+	);
+
+EOF
+# Create the database
+#mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+
+# Create a database user and grant privileges
+#mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
+#mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
+#mysql -e "FLUSH PRIVILEGES;"
+
+# Optional: Print information
+echo "MariaDB enabled, database '$DB_NAME' created, and user '$DB_USER' granted privileges."
+
