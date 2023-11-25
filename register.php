2fed2a9d7e33b3b1c393e31b98cec20e3abc68b4
mohamad0880218 <115563288+mohamad0880218@users.noreply.github.com>
2023-11-25 01:13:41 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection
 $servername = "db:3306"; // Change this if your database is hosted elsewhere
-$username = "root"; // Change this to your database username
-$password = " "; // Change this to your database password
-$dbname = "mysql"; // Change this to your database name
+$username = "username"; // Change this to your database username
+$password = "password"; // Change this to your database password
+$dbname = "website"; // Change this to your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
